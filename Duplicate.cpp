6957ed7e9ecdc98eecafa9313afb30b5b8b08e59
Vishnu Vardhan <vishva777002@gmail.com>
2026-09-21 14:59:24 +0530

    21-09-2026
    Implement a program to check for duplicate integers in input.

---
+++ Duplicate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    
+   vector<int>nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    
+   set<int>s;
+    bool f=true;
+    for(int x:nums){
+        if(s.contains(x)){
+          bool f=false;
+        }
+        s.insert(x);
+    }
+    if(f) cout<<"True"<<endl;
+    else cout<<"False"<<endl;
+    return 0;
+}
